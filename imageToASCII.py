105da7e5159bc187058054f94d00724594887be1
kn1ght0108 <94855920+kn1ght0108@users.noreply.github.com>
2022-11-09 12:27:36 +0700

    Update imageToASCII.py

--- imageToASCII.py
+++ imageToASCII.py
@@ -2,18 +2,18 @@ import sys
 import math
 from PIL import Image, ImageStat
 
-def imgToASCII(image, resolution):
+def imgToASCII(image, num_pixels):
     width, height = image.size
-    rows = math.floor(height/resolution)
-    cols = math.floor(width/resolution)
+    rows = math.floor(height/num_pixels)
+    cols = math.floor(width/num_pixels)
     ascii_scale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1}{[]?-_+~<>i!lI;:,\"^`'. "
     ascii_txt = ""
     for row in range(rows):
-        y_upper = row * resolution
-        y_lower = y_upper + resolution
+        y_upper = row * num_pixels
+        y_lower = y_upper + num_pixels
         for col in range(cols):
-            x_upper = col * resolution
-            x_lower = x_upper + resolution
+            x_upper = col * num_pixels
+            x_lower = x_upper + num_pixels
             tile = image.crop((x_upper,y_upper,x_lower,y_lower))
             avg_lum = ImageStat.Stat(tile)._getmean()
             asciival = ascii_scale[math.ceil(((avg_lum[0]*69)/255))]
@@ -22,7 +22,7 @@ def imgToASCII(image, resolution):
     return ascii_txt
 
 image_path = sys.argv[1]
-resolution = int(sys.argv[2])
+num_pixels = int(sys.argv[2])
 image = Image.open(image_path).convert("L")
-print(imgToASCII(image, resolution))
+print(imgToASCII(image, num_pixels))
 
